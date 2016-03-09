a814d1ce6907060bfba555048761d5a057cf61b7
michaelROG <yuhuaiwu0919@gmail.com>
2016-03-08 21:48:23 -0500

    Code change to make template more generic and add some more bg imgs

--- script/controller.js
+++ script/controller.js
@@ -1,57 +1,45 @@
-var gpsApp = angular.module('gpsApp', ['xml']);
-      
-gpsApp.config(function($httpProvider){
-	$httpProvider.interceptors.push('xmlHttpInterceptor');
-})
-.controller('gpsCtrl',['$scope','$http', function ($scope,$http) {
+var gpsApp = angular.module('gpsApp', [  ]);
 
- 
-  $http.get('../json/gps.json').success(function(data){
-	  $scope.data=data;
-  })
-  
-  $scope.isBank=true;
-  $scope.isUtility=false;
-  $scope.isFrequent=false;
-  $scope.isOther=false;
-  
-  $scope.isShowMore=false;
-  
-  $scope.switchToBank=function(){
-	  $scope.isBank=true;
-	  $scope.isUtility=false;
-	  $scope.isFrequent=false;
-	  $scope.isOther=false;
-  }
-  $scope.swtichToThought=function(){
-	  $scope.isBank=false;
-	  $scope.isUtility=true;
-	  $scope.isFrequent=false;
-	  $scope.isOther=false;
-  }
-  $scope.switchToFrequent=function(){
-	  $scope.isBank=false;
-	  $scope.isUtility=false;
-	  $scope.isFrequent=true;
-	  $scope.isOther=false;
-  }
-  $scope.switchToOther=function(){
-	  $scope.isBank=false;
-	  $scope.isUtility=false;
-	  $scope.isFrequent=false;
-	  $scope.isOther=true;
-  }
-  $scope.showMore=function(){
-	  $scope.isShowMore=true;
-  }
-  $scope.showLess=function(){
-	  $scope.isShowMore=false;
-  }
-  
-  /*  //options to use XML to store data and read data from xml
-   * $http.get('../xml/info.xml').success(function (response) {
-       $scope.zhihu = response.content.blogs.zhihus.zhihu;
-       $scope.otherBlogs=response.content.others.other;
-     });*/
-  
-}]);
+
+gpsApp.controller('gpsCtrl', [ '$scope', '$http', function($scope, $http) {
+
+	 $http.get('../json/gps.json').success(function(data) {
+		$scope.data = data;
+		console.log($scope.data);
+	})
+
+	$scope.isTabOne = true;
+	$scope.isTabTwo = false;
+	$scope.isTabThree = false;
+	$scope.isTabFour = false;
+
+	$scope.isShowMore = false;
+
+	$scope.switchToTabOne = function() {
+		$scope.isTabOne = true;
+		$scope.isTabTwo = false;
+		$scope.isTabThree = false;
+		$scope.isTabFour = false;
+	}
+	$scope.switchToTabTwo = function() {
+		$scope.isTabOne = false;
+		$scope.isTabTwo = true;
+		$scope.isTabThree = false;
+		$scope.isTabFour = false;
+	}
+	$scope.switchToTabThree = function() {
+		$scope.isTabOne = false;
+		$scope.isTabTwo = false;
+		$scope.isTabThree = true;
+		$scope.isTabFour = false;
+	}
+	$scope.switchToTabFour = function() {
+		$scope.isTabOne = false;
+		$scope.isTabTwo = false;
+		$scope.isTabThree = false;
+		$scope.isTabFour = true;
+	}
+
+
+
+} ]);
